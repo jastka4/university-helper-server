e4445e597f159837f51fa8feba8fbbf8ac051517
Justyna Skalska <jastka4@gmail.com>
2020-01-20 13:56:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Server for university helper mobile application
---
+++ src/main/java/org/jastka4/university_helper_server/UniversityHelperServerApplication.java
@@ -0,0 +1,13 @@
+package org.jastka4.university_helper_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityHelperServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityHelperServerApplication.class, args);
+    }
+
+}
